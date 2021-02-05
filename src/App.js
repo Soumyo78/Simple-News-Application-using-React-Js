1dd76ce1a7cc73de4f141c71c3e4d1ad4198026c
Soumyo78 <soumyo78@yahoo.com>
2021-02-05 18:10:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NewsCard from './Components/NewCard/index';
 
-function App() {
+class App extends React.Component{
+
+
+ render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div>Hello Testing</div>
+      <NewsCard/>
     </div>
   );
+ }
 }
 
 export default App;
---
+++ src/Components/NewCard/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+const NewsCard = () =>{
+    return(
+        <h1>Soumyo</h1>
+    );
+}
+
+export default NewsCard;
